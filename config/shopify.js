954cd5a187ba106fa5a2465c25bfaf298c810d97
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2024-12-25 05:21:26 +0200

    Update shopify.js

--- config/shopify.js
+++ config/shopify.js
@@ -1,1 +1,13 @@
+import '@shopify/shopify-api/adapters/node';
+import { shopifyApi, LATEST_API_VERSION } from '@shopify/shopify-api';
 
+const shopify = shopifyApi({
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET,
+  scopes: ['read_orders', 'write_orders', 'write_files'],
+  hostName: process.env.SHOPIFY_SHOP_NAME,
+  apiVersion: LATEST_API_VERSION,
+  isEmbeddedApp: true,
+});
+
+export default shopify;
