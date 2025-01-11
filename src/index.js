baf35f18d3381c4406e1fc569aeea7a76e3ea46c
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2025-01-11 20:10:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import shopify, { generatePurchaseOrder } from '../config/shopify.js';
+import cors from 'cors';
+import connectDB from '../config/database.js';
+import { generatePurchaseOrders } from './services/po-generator.js';
+import supplierRoutes from './routes/suppliers.js';
+import webhookRoutes from './routes/webhooks.js';
 
 dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/api/suppliers', supplierRoutes);
+app.use('/webhooks', webhookRoutes);
 
 app.post('/webhooks/order/create', async (req, res) => {
   try {
     console.log('Order webhook received');
     const order = req.body;
     
-    await generatePurchaseOrder(order);
+    const purchaseOrders = await generatePurchaseOrders(order);
+    
+    // Notify merchant if any POs need approval
+    const needsApproval = purchaseOrders.some(po => po.approvalRequired);
+    if (needsApproval) {
+      // TODO: Implement merchant notification
+      console.log('POs pending approval:', purchaseOrders.map(po => po.poNumber));
+    }
     
     res.status(200).send('OK');
   } catch (error) {
@@ -24,7 +43,7 @@ app.post('/webhooks/order/create', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Cycle3 Shopify PO Automation Running');
+  res.send('Multi-Supplier Management App Running');
 });
 
 app.listen(port, () => {
