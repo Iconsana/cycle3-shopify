8d5b1d901022eb60bda53ea8a2548e7d5f07d8f4
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2025-02-20 22:23:43 +0200

    Update shopify.js

--- config/shopify.js
+++ config/shopify.js
@@ -1,22 +1,20 @@
 import '@shopify/shopify-api/adapters/node';
-import { shopifyApi, LATEST_API_VERSION, AuthScopes } from '@shopify/shopify-api';
-import { restResources } from '@shopify/shopify-api/rest/admin/2024-01';
+import { shopifyApi, LATEST_API_VERSION } from '@shopify/shopify-api';
 
 const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
   scopes: [
-    AuthScopes.ReadProducts,
-    AuthScopes.WriteProducts,
-    AuthScopes.ReadOrders,
-    AuthScopes.WriteOrders,
-    'read_files',
-    'write_files'
+    'read_products',
+    'write_products',
+    'read_orders',
+    'write_orders',
+    'read_inventory',
+    'write_inventory'
   ],
   hostName: process.env.APP_URL.replace(/https:\/\//, ''),
   apiVersion: LATEST_API_VERSION,
   isEmbeddedApp: true,
-  restResources,
 });
 
 export default shopify;
